58f32725ce80f7918667a9e9413bb4b03ae8ae00
ikekim <ik9421217@nutrien.com>
2022-02-03 23:35:17 -0500

    refactored

--- Jenkinsfile7_vars.groovy
+++ Jenkinsfile7_vars.groovy
@@ -6,7 +6,7 @@ pipeline {
     stages {
         stage('Get Project') {                        
             steps {
-                bqTableNamesVars.generateTables(vars.yml)
+                bqTableNamesVars(vars.yml)
             }
         }
     }
