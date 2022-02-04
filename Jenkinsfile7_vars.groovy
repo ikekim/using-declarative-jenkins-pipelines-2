642b683ccdbba1765ff1724840f19b39808f3880
ikekim <ik9421217@nutrien.com>
2022-02-03 23:30:54 -0500

    refactored

---
+++ Jenkinsfile7_vars.groovy
@@ -0,0 +1,13 @@
+library identifier: 'jenkins-pipeline-demo-library-2@main', 
+        retriever: modernSCM([$class: 'GitSCMSource', remote: 'https://github.com/ikekim/jenkins-pipeline-demo-library-2.git'])
+
+pipeline {
+    agent any
+    stages {
+        stage('Get Project') {                        
+            steps {
+                bqTableNamesVars.generateTables(vars.yml)
+            }
+        }
+    }
+}
