5c26184e5ead8a49da29a7a56baf1dd980330d58
ikekim <ik9421217@nutrien.com>
2022-02-04 00:59:25 -0500

    refactored

--- Jenkinsfile8_readconfig.groovy
+++ Jenkinsfile8_readconfig.groovy
@@ -6,7 +6,7 @@ pipeline {
     stages {
         stage('Get Project') {                        
             steps {
-                bqTableNamesVars(vars.yml)
+                readConfigFileProperties.getConfigFileProperties('vars.yml')
             }
         }
     }
